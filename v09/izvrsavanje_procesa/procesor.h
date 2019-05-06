4a99aeb5be6923344aa0bd8e5035e2b5bca28acb
l4v <ivos.jovan@protonmail.ch>
2019-05-06 08:00:27 +0200

    izvrsavanje_procesa

--- v09/izvrsavanje_procesa/procesor.h
+++ v09/izvrsavanje_procesa/procesor.h
@@ -1,6 +1,8 @@
 #ifndef PROCESOR_H_INCLUDED
 #define PROCESOR_H_INCLUDED
 
+#include <mutex>
+#include <condition_variable>
 #include "dijagnostika.h"
 
 using namespace std;
@@ -8,10 +10,15 @@ using namespace std;
 class Procesor {
 private:
     Dijagnostika& dijagnostika;
+    mutex m;
+    bool prekid, procesor_zauzet;
+    condition_variable cv_obradjivac_prekida;
+    condition_variable cv_procesi;
 
 public:
     Procesor(Dijagnostika& d) : dijagnostika(d) {
-        // Proširiti po potrebi...
+        prekid = false;
+        procesor_zauzet = false;
     }
 
     // Pomoćna metoda koja vraća referencu na objekat Dijagnostika asociran sa ovim objektom
@@ -28,7 +35,29 @@ public:
     // Kada proces uspe da zauzme procesor, pre izvršenja naredbe treba da se pozove dijagnostika.proces_izvrsava.
     // Nakon što je proces izvršio naredbu, potrebno je pozvati dijagnostika.proces_zavrsio.
     void izvrsi_proces(int id, int broj_naredbi) {
-        // Implementirati ...
+        for(int i = 0; i < broj_naredbi; ++i){
+            unique_lock<mutex> lock(m);
+            while(procesor_zauzet){
+                dijagnostika.proces_ceka(id);
+                cv_procesi.wait(lock);
+            }
+            dijagnostika.proces_izvrsava(id, i);
+            procesor_zauzet = true;
+
+            lock.unlock();
+            this_thread::sleep_for(chrono::seconds(1));
+            lock.lock();
+
+            dijagnostika.proces_zavrsio(id, i);
+            procesor_zauzet = false;
+            if(prekid)
+                cv_obradjivac_prekida.notify_one();
+            else
+                cv_procesi.notify_one();
+            lock.unlock();
+            this_thread::sleep_for(chrono::milliseconds(100));
+            lock.lock();
+        }
     }
 
     // Metoda koju poziva nit koja simulira obrađivač U/I prekida kako bi se inicirao i obradio prekid.
@@ -37,7 +66,20 @@ public:
     // Kada obrađivač uspe da zauzme procesor, pre obrade prekida treba da se pozove dijagnostika.obradjivac_izvrsava.
     // Nakon što je obrađivač završio obradu, potrebno je pozvati dijagnostika.obradjivac_zavrsio.
     void prekini() {
-        // Implementirati ...
+        unique_lock<mutex> lock(m);
+        prekid = true;
+        while(procesor_zauzet){
+            dijagnostika.obradjivac_ceka();
+            cv_obradjivac_prekida.wait(lock);
+        }
+        dijagnostika.obradjivac_izvrsava();
+        procesor_zauzet = true;
+        lock.unlock();
+        this_thread::sleep_for(chrono::milliseconds(100));
+        lock.lock();
+        procesor_zauzet = false;
+        dijagnostika.obradjen_prekid();
+        cv_procesi.notify_one();
     }
 };
 
