66ec1ad1a672f043c4ae833f2ea42358015256da
l4v <ivos.jovan@gmail.com>
2019-05-26 16:52:17 +0200

    Uradjene V10

--- v10/sazimanje/radna_memorija.h
+++ v10/sazimanje/radna_memorija.h
@@ -15,9 +15,9 @@ private:
     Dijagnostika& dijagnostika;
     mutex m;
     condition_variable cv_proces, cv_sazimanje;
-    bool sazimanje;
+    bool sazima;
     int slobodan_prostor;
-    vector<char> memorija;
+    vector<int> memorija;
 
 
     /*
@@ -25,17 +25,23 @@ private:
     */
     int pocetak_bloka(int velicina){
         int pocetak = 0;
-
-        for(auto it = memorija.begin(); it != memorija.end(); it++){
-
+        int uzastopni = 0;
+        for(auto it = memorija.begin(); it != memorija.end(); it++, pocetak++){
+            uzastopni = *it == -1 ? ++uzastopni : 0;
+            if(uzastopni == velicina){
+                return pocetak - velicina + 1;
+            }
         }
+        return -1;
     }
 
 public:
     Radna_memorija(Dijagnostika& d, int ukupno_lokacija) : dijagnostika(d) {
         slobodan_prostor = ukupno_lokacija;
         memorija.resize(ukupno_lokacija);
-        sazimanje = false;
+        for(auto it = memorija.begin(); it != memorija.end(); it++)
+            *it = -1;
+        sazima = false;
     }
 
     Dijagnostika& getDijagnostika() {
@@ -53,14 +59,72 @@ public:
     // Kada se proces završi, potrebno je pozvati metodu dijagnostika.proces_zavrsio.
     // Metodu dijagnostika.ispisi_memoriju potrebno je pozvati u sledećim momentima: nakon što proces zauzme memoriju i nakon što proces oslobodi memoriju.
     void koristi(int id_procesa, int br_lokacija_procesa, int trajanje) {
-        // Implementirati ...
+        unique_lock<mutex> lock(m);
+        int pocetak;
+        // Ulazi u cekanje ukoliko ne postoji dovoljno velik blok ili je sazimanje u toku
+        while((pocetak = pocetak_bloka(br_lokacija_procesa)) == -1 || sazima){
+            dijagnostika.proces_ceka(id_procesa);
+            cv_proces.wait(lock);
+        }
+        dijagnostika.proces_krenuo(id_procesa, br_lokacija_procesa);
+
+        // Zauzima memoriju
+        for(int i = 0; i < br_lokacija_procesa; i++)
+            memorija[i+pocetak] = id_procesa;
+
+        dijagnostika.ispisi_memoriju(memorija.begin(), memorija.end());
+
+        lock.unlock();
+        this_thread::sleep_for(chrono::seconds(trajanje));
+        lock.lock();
+
+        // U slucaju da je doslo do sazimanja u toku izvrsavanja procesa
+        for(pocetak = 0; pocetak < memorija.size(); pocetak++)
+            if(memorija[pocetak] == id_procesa)break;
+
+        // Oslobadja memoriju
+        for(int i = 0; i < br_lokacija_procesa; i++)
+            memorija[i+pocetak] = -1;
+
+        dijagnostika.proces_zavrsio(id_procesa);
+        dijagnostika.ispisi_memoriju(memorija.begin(), memorija.end());
+
+        sazima = true;
+        cv_sazimanje.notify_one();
     }
 
     // Metoda koju poziva nit operativnog sistema koji se bavi sažimanjem memorije
     //
     // Nakon što je sažimanje obavljeno potrebno je pozvati metodu dijagnostika.sazimanje_obavljeno a nakon toga i metodu dijagnostika.ispisi_memoriju kako bi se prikazali efekti sažimanja memorije.
     void sazimanje() {
-        // Implementirati ...
+        unique_lock<mutex> lock(m);
+        while(!sazima)
+            cv_sazimanje.wait(lock);
+        sazima = false;
+
+        /*vector<int>::iterator it_free = memorija.begin(); // iterator za slobodne bajtove
+        for(auto it = memorija.begin(); it != memorija.end(); it++){
+            it_free = *it != -1 ? it_free++ : it_free; // Ide do prvog slobodnog bajta
+            while(*it != -1){
+                *it_free++ = *it;
+                *it++ = -1;
+            }
+        }*/
+
+        vector<int> novi_vektor;
+        novi_vektor.resize(memorija.size(), -1);
+        vector<int>::iterator it_novi = novi_vektor.begin();
+        for(auto it = memorija.begin(); it != memorija.end(); it++)
+            if(*it != -1)
+                *it_novi++ = *it;
+
+        memorija = novi_vektor;
+
+
+        dijagnostika.sazimanje_obavljeno();
+        dijagnostika.ispisi_memoriju(memorija.begin(), memorija.end());
+        cv_proces.notify_all();
+
     }
 };
 
--- v10/virtuelna_memorija/program.h
+++ v10/virtuelna_memorija/program.h
@@ -1,6 +1,9 @@
 #ifndef PROGRAM_H_INCLUDED
 #define PROGRAM_H_INCLUDED
 
+#include <condition_variable>
+#include <mutex>
+
 #include "dijagnostika.h"
 #include "naredba.h"
 #include "povratna_vrednost.h"
@@ -10,10 +13,19 @@ using namespace std;
 class Program {
 private:
     Dijagnostika& dijagnostika;
+    mutex m;
+    int slobodno; // Koliko slobodnog RAM prostora ima
+    int zauzmi_vm; // Koliko treba zauzeti vm
+    int zauzeti_vm; // Koliko je zauzeto
+    bool zauzimanje_vm; // Da li se vrsi zauzimanje virtuelne memorije
+    condition_variable cv_virt; // Signalizira pocetak zauzimanja vm
+    condition_variable cv_zavrseno; // Signalizira zavrsetak zauzimanja vm
 
 public:
     Program(Dijagnostika& d, int kapacitet) : dijagnostika(d) {
-        // Proširiti po potrebi ...
+        zauzimanje_vm = false;
+        slobodno = kapacitet;
+        zauzeti_vm = 0;
     }
 
     Dijagnostika& getDijagnostika() {
@@ -25,13 +37,53 @@ public:
     // naredba - naredba koja se izvršava (naredba kojom se zauzima memorija)
     // Vraća instancu klase Povratna_vrednost koja sadrži opis zauzeća memorije NAKON izvršenja naredbe.
     Povratna_vrednost izvrsi_naredbu(Naredba naredba) {
-        // Implementirati ...
+        unique_lock<mutex> lock(m);
+        int alocirani_vm = 0;
+        if(naredba.tip.compare("repeat")){ // compare vraca 0 ako su isti
+            // Ovde ulazi ako nije repeat
+            if(naredba.kolicina_memorije <= slobodno){
+                // Ovde ulazi ako moze sve u RAM
+                slobodno -= naredba.kolicina_memorije;
+            }else{
+                zauzmi_vm = naredba.kolicina_memorije;
+                zauzimanje_vm = true;
+                cv_virt.notify_one();
+                while(zauzimanje_vm)
+                    cv_zavrseno.wait(lock);
+            }
+        }else{
+            // Ovde ulazi ako je repeat
+            for(int i = 0; i < naredba.ponavljanja; i++){
+                if(naredba.kolicina_memorije > slobodno){
+                    zauzmi_vm = naredba.kolicina_memorije;
+                    zauzimanje_vm = true;
+                    cv_virt.notify_one();
+                    while(zauzimanje_vm)
+                        cv_zavrseno.wait(lock);
+                }
+            }
+        }
+
+
+        return {slobodno, zauzeti_vm};
     }
 
     // Metoda koju poziva nit koja simulira deo operativnog sistema koji se bavi zauzimanjem virtuelne memorije kako bi se zauzela određena količina VM
     // Potrebnu količinu VM (kao i bilo koju drugu neophodnu informaciju) preneti u nekom izabranom polju klase.
     int zauzmi_virtuelnu_memoriju() {
-        // Implementirati ...
+        unique_lock<mutex> lock(m);
+        while(!zauzimanje_vm)
+            cv_virt.wait(lock);
+
+
+        zauzeti_vm += zauzmi_vm;
+        zauzmi_vm = 0;
+
+        this_thread::sleep_for(chrono::seconds(1));
+        zauzimanje_vm = false;
+        cv_zavrseno.notify_one();
+
+        return zauzeti_vm;
     }
 };
 
